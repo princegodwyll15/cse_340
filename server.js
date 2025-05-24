4f840dc92b20d1e95811627d931ab7933bf45f55
princegodwyll15 <godwyllprince@gmail.com>
2025-05-24 15:30:10 +0000

    car types and design done

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const baseController = require("./controllers/baseController");
 const static = require("./routes/static");
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities");
 
 /* ***********************
  * View Engine and Templates
@@ -25,8 +26,13 @@ app.set("layout", "./layouts/layout");
  *************************/
 app.use(static);
 app.use("/inv", inventoryRoute);
-//Index route
-app.get("/", baseController.buildHome);
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
 
 /* ***********************
  * Local Server Information
@@ -34,6 +40,29 @@ app.get("/", baseController.buildHome);
  *************************/
 const port = process.env.PORT;
 const host = process.env.HOST;
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
+});
 
 /* ***********************
  * Log statement to confirm server operation
